8fb304efdda94daff140d10910f955dd46789d0f
Cybergate <cybergatenet@yahoo.com>
2022-03-24 23:46:10 -0400

    structured the server files

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express()
-const PORT = 3011 || process.env.PORT
+const PORT = 3011 || process.env.PORT;
+const path = require('path')
 
 
+// app.use(express.static('public'))
+// app.use('/static', express.static('public'))
+// app.use(express.static('files'))
+app.use(express.static(__dirname + 'public'))
 
 app.get('/', (req, res) => {
-    res.send('welcome');
+    // app.use('/static', express.static(path.join(__dirname, 'public')))
+    res.sendFile(path.join(__dirname, '../public', './index.html'))
 })
 
 app.listen(PORT, () => console.log(`App running on port ${PORT}`))
